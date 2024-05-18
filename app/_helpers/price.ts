eb14e7013b5f30cbeb97048c4f46cab7088d95e1
lucasregisdemoraes <lucasgrm03@gmail.com>
2024-05-18 11:44:16 -0300

    feat: add product list and items

---
+++ app/_helpers/price.ts
@@ -0,0 +1,22 @@
+import { Product } from "@prisma/client";
+
+export const calculateProductTotalPrice = (product: Product): number => {
+  if (product.discountPercentage === 0) {
+    return Number(product.price);
+  }
+
+  const discount =
+    Number(product.price) * Number(product.discountPercentage / 100);
+
+  return Number(product.price) - discount;
+};
+
+export const formatCurrency = (value: number): string => {
+  return (
+    "R$ " +
+    Intl.NumberFormat("pt-BR", {
+      currency: "BRL",
+      minimumFractionDigits: 2,
+    }).format(Number(value))
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
+import { ChevronRight } from "lucide-react";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
+import ProductList from "./_components/product-list";
 import PromoBanner from "./_components/promo-banner";
 import Search from "./_components/search";
+import { Button } from "./_components/ui/button";
+// import { db } from "./_lib/prisma";
 
-const Home = () => {
+// const products = db.product.findMany({
+//   where: {
+//     discountPercentage: {
+//       gt: 0,
+//     },
+//   },
+//   take: 10,
+//   include: {
+//     restaurant: {
+//       select: {
+//         name: true,
+//       },
+//     },
+//   },
+// });
+
+const Home = async () => {
   return (
     <>
       <Header />
@@ -20,6 +40,20 @@ const Home = () => {
           src="/promo-banner-01.png"
           alt="30% de desconto em pizzas!"
         />
+      </div>
+
+      <div className="pt-6">
+        <div className="mb-4 flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos recomendados</h2>
+          <Button
+            className="flex h-fit items-center p-0 text-primary hover:bg-transparent"
+            variant="ghost"
+          >
+            <span className="text-xs">Ver todos</span>
+            <ChevronRight size={16} />
+          </Button>
+        </div>
+        <ProductList />
       </div>
     </>
   );
